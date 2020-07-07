0401aa4f5cf7e3c1da7488c149c889d7495292e2
gitmatheo <mateusz.dominiak@stxnext.pl>
2020-07-07 13:31:21 +0200

    complete the first module

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { response } = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.render('index', { title: 'Index' });
+});
+
+app.listen(3000, () => {
+  console.log('PS Project Running on port 3000!');
+});
