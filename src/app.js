b9231e97802fc894c80dcc23121e51ddecb6bace
gitmatheo <mateusz.dominiak@stxnext.pl>
2020-07-07 23:25:14 +0200

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,27 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { response } = require('express');
 
 const app = express();
+const { accounts, users, writeJSON } = require('./data.js');
 const accountRoutes = require('./routes/accounts.js');
 const servicesRoutes = require('./routes/services.js');
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/services', servicesRoutes);
-app.use('/accounts', accountRoutes);
-
-// fs.readFileSync("src/json/accounts.json", "UTF-8")
-
-const { accounts, users, writeJSON } = require('./data.js');
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
   response.render('index', { title: 'Account Summary', accounts: accounts });
 });
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (request, response) => {
   response.render('profile', { user: users[0] });
