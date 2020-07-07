45eca625a7958ec2510a78bb60a7a136149e542f
gitmatheo <mateusz.dominiak@stxnext.pl>
2020-07-07 21:28:50 +0200

    complete module 3

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { response } = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -35,6 +36,40 @@ app.get('/credit', (request, response) => {
 
 app.get('/profile', (request, response) => {
   response.render('profile', { user: users[0] });
+});
+
+app.get('/transfer', (request, response) => {
+  response.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+  let accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, 'json', 'accounts.json'),
+    accountsJSON,
+    'utf8'
+  );
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) =>
+  res.render('payment', { account: accounts.credit })
+);
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount);
+  let accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, 'json', 'accounts.json'),
+    accountsJSON,
+    'utf8'
+  );
+  res.render('payment', {
+    message: 'Payment Successful',
+    account: accounts.credit,
+  });
 });
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
