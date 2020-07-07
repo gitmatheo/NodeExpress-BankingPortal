eb3f86ed29a69416b43c7d44e497685c6f57ca53
gitmatheo <mateusz.dominiak@stxnext.pl>
2020-07-07 19:26:06 +0200

    complete module 2

--- src/app.js
+++ src/app.js
@@ -9,10 +9,33 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// fs.readFileSync("src/json/accounts.json", "UTF-8")
+
+const accountData = fs.readFileSync('src/json/accounts.json', 'UTF-8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'UTF-8');
+const users = JSON.parse(userData);
+
 app.get('/', (request, response) => {
-  response.render('index', { title: 'Index' });
+  response.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
+app.get('/savings', (request, response) => {
+  response.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (request, response) => {
+  response.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (request, response) => {
+  response.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (request, response) => {
+  response.render('profile', { user: users[0] });
+});
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
 });
